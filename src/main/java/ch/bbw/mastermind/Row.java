ea82e25d071896cf56f229180cc3cb8d7cba2805
Henry Joerg <henry.joerg@gmail.com>
2021-06-14 13:56:44 +0200

    tmp commit

---
+++ src/main/java/ch/bbw/mastermind/Row.java
@@ -0,0 +1,25 @@
+package ch.bbw.mastermind;
+
+public class Row {
+    int[] codePegs;
+    int[] keyPegs;
+
+    public Row() {
+    }
+
+    public int[] getCodePegs() {
+        return codePegs;
+    }
+
+    public void setCodePegs(int[] codePegs) {
+        this.codePegs = codePegs;
+    }
+
+    public int[] getKeyPegs() {
+        return keyPegs;
+    }
+
+    public void setKeyPegs(int[] keyPegs) {
+        this.keyPegs = keyPegs;
+    }
+}
