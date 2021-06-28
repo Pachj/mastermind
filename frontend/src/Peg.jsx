87d5a9972daeb413583f8a7bd194798bd4ab54f5
Henry Joerg <henry.joerg@gmail.com>
2021-06-28 14:03:46 +0200

    js files changed to jsx files

--- frontend/src/Peg.jsx
+++ frontend/src/Peg.jsx
@@ -1,15 +1,12 @@
-import React from "react";
+import React from 'react';
 
 // TODO: disable keyPegs
 export class Peg extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <button
         style={{ background: this.props.bgColor }}
-        className={"peg"}
+        className={'peg'}
         onClick={this.props.setPeg}
       />
     );
